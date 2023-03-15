56b8d0ec259dd55c24330e7cbf9d022d4887d41c
Simon Bäckström (WorkLaptop) <simon@aurorapunks.com>
2023-03-15 12:07:58 +0100

    Added MonoBehaviorExtensions

---
+++ Runtime/Utility/MonoBehaviorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace SimonB.Core.Utility
+{
+    public static class MonoBehaviorExtensions
+    {
+        public static void DestroyGameObject(this MonoBehaviour monoBehaviour) {
+            if(monoBehaviour == null) return;
+            Object.Destroy(monoBehaviour.gameObject);
+        }
+    }
+}
